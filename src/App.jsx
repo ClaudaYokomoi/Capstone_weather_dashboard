8415db12e78f119ccd547eb19490542a265e3ddd
Clauda Yokomoi <claudayokomoi@gmail.com>
2024-10-13 23:03:14 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import ConvertForm from "../../capstone_weather_dashboard/src/components/ConvertForm";
+
+const App = () => {
+  return (
+    <div className="currency-converter">
+      <h2 className="converter-title">Currency Converter</h2>
+     <ConvertForm />
+    </div>
+  );
+};
+
+export default App;
